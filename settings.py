7110aa5e4347d623802041655aa30112b551d8b8
Thejesh GN <i@thejeshgn.com>
2013-01-14 14:08:00 +0530

    initial project structure

---
+++ settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+#: The title of this site
+SITE_TITLE='Data View'
+#: Support contact email
+SITE_SUPPORT_EMAIL = 'arghyam@thejeshgn.com'
+#: TypeKit code for fonts
+TYPEKIT_CODE=''
+#: Google Analytics code UA-XXXXXX-X
+GA_CODE=''
+
+#: Google Maps code 
+GMAPS_CODE=''
+
+#: Database backend
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+#: Secret key
+SECRET_KEY = 'make this something random'
+#: Timezone
+TIMEZONE = 'Asia/Calcutta'
+#: Log file
+LOGFILE='error.log'
+
+DATABASE = './database/test.db'
